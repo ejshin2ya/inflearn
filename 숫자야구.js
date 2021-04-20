50597d5ac2574ab045d787e87fbb65d50e4eb642
eunjeong <lovekiej92@gmail.com>
2021-04-20 12:06:48 +0900

    practice2

--- 숫자야구.js
+++ 숫자야구.js
@@ -1,12 +1,15 @@
 var 바디 = document.body;
-
-var 숫자후보 = [1,2,3,4,5,6,7,8,9];
-var 숫자배열 = [];
-
-for (var i = 0; i<4; i +=1 ) {
-    var 뽑은것 = 숫자후보.splice(Math.floor(Math.random()*(9-i)),1)[0];
-    숫자배열.push(뽑은것);
+var 숫자후보;
+var 숫자배열 ;
+function 숫자뽑기(){
+    숫자후보 = [1,2,3,4,5,6,7,8,9];
+    숫자배열 = [];
+    for (var i = 0; i<4; i +=1 ) {
+        var 뽑은것 = 숫자후보.splice(Math.floor(Math.random()*(9-i)),1)[0];
+        숫자배열.push(뽑은것);
+    }
 }
+숫자뽑기();
 console.log(숫자배열);
 var 결과 = document.createElement('h1');
 바디.append(결과);
@@ -20,13 +23,49 @@ var 버튼 = document.createElement('button');
 폼.append(버튼);
 버튼.textContent='입력!';
 
-폼.addEventListener('submit', function 비동기(이벤트){
+var 틀린횟수=0;
+
+폼.addEventListener('submit', function 비동기(이벤트){//엔터를 쳤을때
     이벤트.preventDefault();
     var 답 = 입력창.value;
-    console.log(답);
-    if(답 === 숫자배열.join('')){
+    if(답 === 숫자배열.join('')){//답이 맞으면
+    결과.textContent = '홈런';
+    입력창.value='';
+    입력창.focus();
 
-    } else{
-        
+    숫자뽑기();
+
+    틀린횟수 =0;
+    } else{//답이 틀리면
+    var 답배열 = 답.split('');
+    var 스트라이크 = 0;
+    var 볼 =0;
+
+    틀린횟수+=1
+    if(틀린횟수>10){//10번 넘게 틀린경우
+        결과.textContent='10번 넘게 틀려서 실패! 답은 '+ 숫자배열.join(',')+' 였습니다.!';
+        입력창.value='';
+        입력창.focus();
+
+        숫자뽑기();
+
+        틀린횟수=0;
+    } else {//10번 미만으로 틀린경우
+        console.log('답이 틀리면', 답배열);
+    for(var i= 0; i<3; i +=1){//같은 자리인지 확인
+     if(Number(답배열[i])===숫자배열[i]){
+        console.log('같은자리?');
+        스트라이크 +=1;
+     }else if(숫자배열.indexOf(Number(답배열[i]))>-1){//같은 자리는 아니지만, 숫자가 겹치는지 확인
+        console.log('겹치는 숫자');
+        볼 +=1;
+     }
+    }
+     결과.textContent = 스트라이크 + ' 스트라이크 ' +볼+' 볼입니다.';  
+     입력창.value='';
+     입력창.focus();
+
+    }
+    
     }
 });
